bf65edf6a9e4e38ecd3262724b00db195599f994
Suhaib Malik <suhaibmalik6397@gmail.com>
2023-03-09 23:56:53 +0530

    Updated SecurityConfig

--- Backend/todo/src/main/java/com/masai/Controller/UserController.java
+++ Backend/todo/src/main/java/com/masai/Controller/UserController.java
@@ -1,5 +1,26 @@
 package com.masai.Controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
 public class UserController {
+
+    @GetMapping("/register")
+
+    public String registerUser(){
+
+        return "Hello i am User";
+    }
+
+    @GetMapping("/login")
+
+    public String loginUser(){
+
+        return "Hello Login";
+    }
+    
     
 }
